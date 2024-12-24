a800d1cf05e79908ddd4a268e73a4a313f7e0a09
SolTech <soltech2vp@gmail.com>
2024-12-24 13:19:17 +0200

    Add project files.

---
+++ Essencial.Task1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Essencial.Task1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            
+            Console.WriteLine("Введіть довжину прямокутника:");
+            var side1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введіть ширину прямокутника:");
+            var side2 = double.Parse(Console.ReadLine());
+            
+            var rec = new Rectangle(side1, side2);
+            
+            Console.WriteLine($"Розрагунки прямокутника: \nПлоща = {rec.Area}; \nПериметр = {rec.Perimeter}.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Essencial.Task1.2/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essencial.Task1._2
+{
+    internal class Rectangle
+    {
+        private double side1;
+        private double side2;
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        private double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+        private double PerimeterCalculator()
+        {
+            return 2 * (side1 + side2);
+        }
+        public double Area
+        {
+            get { return AreaCalculator(); }
+        }
+        public double Perimeter
+        {
+            get { return PerimeterCalculator(); }
+        }
+    } 
+}
