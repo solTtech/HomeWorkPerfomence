b82ed9889ec3b21df9a890239b86b019c2e2152d
SolTech <soltech2vp@gmail.com>
2025-01-01 23:56:27 +0200

    Fixwed

--- Task5.3/MyMatrix.cs
+++ Task5.3/MyMatrix.cs
@@ -28,5 +28,56 @@ namespace Task5._3
                 }
             }
         }
+        // Метод для зміни розміру матриці
+        public void ResizeMatrix(int newRows, int newColums)
+        {
+            int[,] newMatrix = new int[newRows, newColums];
+            for (int i = 0; i < newRows; i++)
+            {
+                for (int j = 0; j < newColums; j++)
+                {
+                    if (i < matrix.GetLength(0) && j < matrix.GetLength(1))
+                    {
+                        newMatrix[i, j] = matrix[i, j];
+                    }
+                    else
+                    {
+                        newMatrix[i, j] = 0;
+                    }
+                }
+            }
+            matrix = newMatrix;
+        }
+
+        // Виведення матриці на екран
+        public void PrintMatrix()
+        {
+            for (int i = 0;i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0;j < matrix.GetLength(1); j++)
+                {
+                    Console.WriteLine(matrix[i, j] + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        // Метод отримання похідної матриці
+
+        public MyMatrix GetSubMatrix(int order)
+        {
+            if ( order > matrix.GetLength(0) || order > matrix.GetLength(1))
+            {
+                throw new ArgumentException("Порядок більше розмірів матриці.");
+            }
+            MyMatrix subMatrix = new MyMatrix(order, order);
+
+            for (int i = 0; i < order; i++)
+            {
+                for ( int j = 0; j < order; j++)
+                    subMatrix.matrix[i, j] = matrix[i, j];
+            }
+            return subMatrix;
+        }
     }
 }
--- Task5.3/Program.cs
+++ Task5.3/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            MyMatrix myMatrix = new MyMatrix(3, 3);
+
+            Console.WriteLine("Початкова матриця");
+            myMatrix.PrintMatrix();
+
+            //Зміна розміру матнриці
+            myMatrix.ResizeMatrix(4, 6);
+            Console.WriteLine("Змінена матриця");
+            myMatrix.PrintMatrix();
+
+            // Отриання похідної матриці та її виведення
+            Console.WriteLine("Отримуємо матрицю 4 х 4");
+            MyMatrix subMatrix = myMatrix.GetSubMatrix(4);
+            subMatrix.PrintMatrix();
         }
     }
 }
