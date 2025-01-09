89df750e01d19a6c534ee784cfc05b34cce11a95
SolTech <soltech2vp@gmail.com>
2025-01-09 18:58:47 +0200

    Added task8.5

---
+++ Task8.5/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task8._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            Console.WriteLine("Input date of Birth (format mm.dd.yyyy)");
+            if (DateTime.TryParse(Console.ReadLine(), out DateTime birthDate))
+            {
+                
+                DateTime today = DateTime.Now;
+                DateTime nextBirthday = new DateTime(today.Year, birthDate.Month, birthDate.Day);
+
+                if (nextBirthday < today)
+                {
+                    nextBirthday = nextBirthday.AddYears(1);
+                }
+                
+                int daysUntilBirthday = (nextBirthday - today).Days;
+
+                Console.WriteLine($"{daysUntilBirthday} days left until your next birthdayleft until your next birthday.");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect time format.");
+            }
+        }
+    }
+}
