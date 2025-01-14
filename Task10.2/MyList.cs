dc746da62626d1a4c4b21893f1fca0d60e452019
SolTech <soltech2vp@gmail.com>
2025-01-14 15:13:16 +0200

    Added task10.2

---
+++ Task10.2/MyList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10._2
+{
+    class MyList<T>
+    {
+        private T[] arr;
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index <= 0 || index > arr.Length)
+                    throw new IndexOutOfRangeException("Індекс має бути в межах від 1 до arr.Length.");
+                return arr[index - 1];
+            }
+            set
+            {
+                if (index <= 0 || index > arr.Length)
+                    throw new IndexOutOfRangeException("Індекс має бути в межах від 1 до arr.Length.");
+                arr[index - 1] = value;
+            }
+        }
+        
+        
+        public int LengthList => arr.Length;
+
+        public MyList()
+        {
+            arr = new T[0];
+        }
+
+        public void Add(T value)
+        {
+            T[] newArr = new T[arr.Length + 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = arr[i];
+            }
+            newArr[arr.Length] = value;
+            arr = newArr;
+        }
+
+    }
+}
---
+++ Task10.2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task10._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("BMW");
+            myList.Add("AUDI");
+            myList.Add("VW");
+            myList.Add("HYUNDAI");
+            myList.Add("KIA");
+
+
+            Console.WriteLine(myList[4]);
+            Console.WriteLine(myList.LengthList);
+        }
+    }
+}
