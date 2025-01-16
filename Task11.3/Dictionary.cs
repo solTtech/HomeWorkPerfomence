1e64382788754d030c212ae0af42decc33ec265e
SolTech <soltech2vp@gmail.com>
2025-01-16 18:54:23 +0200

    Added task11.3

---
+++ Task11.3/Dictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11._3
+{
+    public class Dictionary<TKey, TValue>
+    {
+        private List<TKey> keys;
+        private List<TValue> values;
+
+        public Dictionary()
+        {
+            keys = new List<TKey>();
+            values = new List<TValue>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            keys.Add(key);
+            values.Add(value);
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                int index = keys.IndexOf(key);
+                return values[index];
+            }
+            set
+            {
+                int index = keys.IndexOf(key);
+                if (index == -1)
+                {
+                    keys.Add(key);
+                    values.Add(value);
+                }
+                else
+                {
+                    values[index] = value;
+                }
+            }
+        }
+
+        public int Count
+        {
+            get { return keys.Count; }
+        }
+    }
+}
---
+++ Task11.3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task11._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, int>();
+
+            dictionary.Add("one", 1);
+            dictionary.Add("two", 2);
+
+            Console.WriteLine("Value for 'one': " + dictionary["one"]);
+            Console.WriteLine("Total count: " + dictionary.Count);
+
+            dictionary["three"] = 3;
+            Console.WriteLine("Value for 'three': " + dictionary["three"]);
+
+            Console.WriteLine("Updated total count: " + dictionary.Count);
+        }
+    }
+}
